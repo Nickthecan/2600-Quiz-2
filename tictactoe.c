b57561b05abb2381ac18e60e65365eb8968a957c
Nicholas Amancio <nick.amancio44@gmail.com>
2022-10-07 16:10:28 -0700

    Added the psuedo code for the project

---
+++ tictactoe.c
@@ -0,0 +1,35 @@
+//Tic Tac Toe game between two players or player vs. cpu
+
+int main() {
+
+    //prompt user if they want solo or multiplayer
+
+    //read information from console
+
+    //play 
+
+    /*for multiplayer
+        show the board
+        allow player one to choose which square they want.
+        populate the square with x
+        update the board
+        allow player two to choose which square they want.
+        populate the square with o
+        update the board
+        repeat until one person gets 3 in a row
+        or all the squares fill up
+    */
+
+    /*for singleplayer
+        show the board
+        allow player to choose which square they want
+        populate the square with x
+        update the board
+        the computer will choose a square by random (make sure it's empty tho)
+        populate the square with o
+        update the board
+        repeat until one person gets 3 in a row
+        or all the squares fill up
+    */
+    return 0;
+}
