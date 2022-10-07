1bdf9a5692c2a10a394f006ceb8aae0b911112c5
Nicholas Amancio <nick.amancio44@gmail.com>
2022-10-07 16:20:31 -0700

    added the methods to print and reset the board whenever necessary

--- tictactoe.c
+++ tictactoe.c
@@ -51,3 +51,20 @@ int main() {
     */
     return 0;
 }
+
+void resetBoard() {
+    for(int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void printBoard() {
+    printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+    printf("\n---|---|---\n");
+    printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+    printf("\n");
+}
